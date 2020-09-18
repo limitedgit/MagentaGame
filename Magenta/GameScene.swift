a86c474ebcc8558333d2a2b3debf5314cd56b2ec
james <jamesw0505protonmail.com>
2020-09-17 21:03:47 -0700

    removed redundant if statement check and changeToPink call in touchesMoved

--- Magenta/GameScene.swift
+++ Magenta/GameScene.swift
@@ -11,26 +11,26 @@ import GameplayKit
 
 let pinkk: UIColor = UIColor(named: "pinkk")!
 class GameScene: SKScene {
-    let black = SKTexture(imageNamed: "playerWhite")
-    let pink = SKTexture(imageNamed: "playerPink")
-    let player = SKSpriteNode(imageNamed:"playerWhite")
-    var gameArea = CGRect()
-    var pause = false
-    let scoreLabel = SKLabelNode(fontNamed: "Comic Sans MS")
-    let levelLabel = SKLabelNode(fontNamed: "Comic Sans MS")
-    var colored = false
-    var level = 0
+    let black = SKTexture(imageNamed: "playerWhite") //black circle with white outline, player sprite default
+    let pink = SKTexture(imageNamed: "playerPink") //pink circle, player sprite when touched
+    let player = SKSpriteNode(imageNamed:"playerWhite")// player sprite
+    var gameArea = CGRect() //area where player stays in
+//    var pause = false   //is the game paused
+    let scoreLabel = SKLabelNode(fontNamed: "Comic Sans MS") //label to display score
+    let levelLabel = SKLabelNode(fontNamed: "Comic Sans MS") //label to display level (second score)
+    var colored = false //whether player is touched (pink)
+    var level = 0 //starting level
     
     struct PhysicsCategories{
-        static let None: UInt32 = 0
-        static let Player: UInt32 = 0b1 //1
-        static let omni: UInt32 = 0b10 //2
-        static let Magenta: UInt32 = 0b100 //4
+        static let None: UInt32 = 0 //no contact
+        static let Player: UInt32 = 0b1 //1 the player
+        static let omni: UInt32 = 0b10 //2 the obstacle that always affects the player
+        static let Magenta: UInt32 = 0b100 //4 the obstacle that only affects the touched player
     }
     
     override init(size: CGSize) {
         super.init(size: size)
-        
+        //setup gamearea
         let maxAspectRatio: CGFloat = 16.0/9.0
         let playableWidth = size.height / maxAspectRatio
         let margin = (size.width - playableWidth) / 2
@@ -42,6 +42,7 @@ class GameScene: SKScene {
     }
     
     override func sceneDidLoad() {
+        //create player
         player.setScale(0.25)
         player.position = CGPoint(x:self.size.width/2, y: self.size.height*0.2)
         player.zPosition = 1
@@ -52,7 +53,7 @@ class GameScene: SKScene {
         self.addChild(player)
         
         
-        
+        //create initial background
         let startGround = SKSpriteNode(imageNamed:"startGround")
         startGround.size = self.size
         startGround.position = CGPoint(x:self.size.width/2, y:self.size.height/2)
@@ -61,7 +62,7 @@ class GameScene: SKScene {
         self.addChild(startGround)
         
         
-        
+        //init score label
         scoreLabel.text = "0"
         scoreLabel.fontSize = 64
         scoreLabel.fontColor = pinkk
@@ -70,6 +71,7 @@ class GameScene: SKScene {
         scoreLabel.zPosition = 1
         self.addChild(scoreLabel)
         
+        //init level label
         levelLabel.text = "level: \(level)"
         levelLabel.fontSize = 64
         levelLabel.fontColor = pinkk
@@ -78,7 +80,7 @@ class GameScene: SKScene {
         self.addChild(levelLabel)
         
         
-        
+        //create moving background
         for i in 1...2 {
             let background = SKSpriteNode(imageNamed: "background")
             background.size = self.size
@@ -91,23 +93,26 @@ class GameScene: SKScene {
 
     }
     
+    // whether the game has not started, started or ended
     enum gameState {
         case pregame
         case ingame
         case postgame
     }
     
-    
+    //change player to pink
     func changeToPink(){
         player.texture = pink
         colored = true
     }
     
+    //change player back to default black with white outline
     func changeToBlack(){
         player.texture = black
         colored = false
     }
     
+    //TODO spawn enemies
     func startGame(){
         let omniEnemy = SKSpriteNode(imageNamed: "omniEnemy")
         omniEnemy.physicsBody = SKPhysicsBody(texture: omniEnemy.texture!, size: omniEnemy.texture!.size())
@@ -116,10 +121,10 @@ class GameScene: SKScene {
         omniEnemy.physicsBody?.collisionBitMask = PhysicsCategories.None
         omniEnemy.physicsBody?.contactTestBitMask = PhysicsCategories.Player | PhysicsCategories.omni | PhysicsCategories.Magenta
     }
-    
+    //start in pregame
     var currentGameState = gameState.pregame
     
-    
+    //variables for moving background and scores
     var lastUpdateTime: TimeInterval = 0
     var frameDeltatime: TimeInterval = 0
     var amountToMovePerSecond = CGFloat(600.0)
@@ -127,25 +132,28 @@ class GameScene: SKScene {
     var currentScore = CGFloat(0.0)
     
     
+    
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
             
         if currentGameState == gameState.pregame{
+            //start the game when player touches screen
             currentGameState = gameState.ingame
             startGame()
         } else if currentGameState == gameState.ingame{
+            //changes colour when touched
             changeToPink()
         }
     }
     
     override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+        //stopping touch changes back to black
         changeToBlack()
     }
     
     override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
         
-        if currentGameState == gameState.ingame{
-            changeToPink()
         for touch in touches {
+            //player follows touch
            let pointOfTouch = touch.location(in:self)
             let previousPointOfTouch = touch.previousLocation(in:self)
             
@@ -168,21 +176,25 @@ class GameScene: SKScene {
                 player.position.y = (gameArea.minY + player.size.height/2)
             }
         }
-        }
     }
     
+    //TODO create game over screen
     func gameOver(){
         
     }
     
+    
+    //display the score
     func startNewLevel(){
-        
+    
         level += 1
         levelLabel.text = "level: \(level)"
 
         
     }
     
+    
+    //TODO: need asset for animation for when player comes into contact with objects
     func spawnExplosion(position: CGPoint) {
         let explosion = SKSpriteNode(imageNamed: "explosion")
         explosion.setScale(0)
@@ -198,6 +210,7 @@ class GameScene: SKScene {
     }
     
 
+    //contact rules for players and enemies
     func didBeginContact(contact: SKPhysicsContact) {
         var body1 =  SKPhysicsBody()
         var body2 =  SKPhysicsBody()
@@ -235,6 +248,7 @@ class GameScene: SKScene {
     
     
     
+    //time based functions
     override func update(_ currentTime: TimeInterval) {
         if lastUpdateTime == 0 {
             lastUpdateTime = currentTime
@@ -243,12 +257,14 @@ class GameScene: SKScene {
             frameDeltatime = currentTime - lastUpdateTime
             lastUpdateTime = currentTime
         }
+        //update score
         currentScore += scorePerSecond * CGFloat(frameDeltatime)
         let amountToMoveBackground = amountToMovePerSecond * CGFloat(frameDeltatime)
         if Int(currentScore).isMultiple(of: 100) && currentScore > CGFloat(level) * 100.0 + 1{
             startNewLevel()
         }
         
+        //move background
         self.enumerateChildNodes(withName: "background") {
             background, stop in
             if self.currentGameState == gameState.ingame{
@@ -261,6 +277,7 @@ class GameScene: SKScene {
                 }
             
         }
+        //move and remove starting background
         self.enumerateChildNodes(withName: "startGround") {
             background, stop in
             if self.currentGameState == gameState.ingame && self.isPaused == false{
